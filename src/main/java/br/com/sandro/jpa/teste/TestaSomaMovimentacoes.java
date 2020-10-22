aa2cf0b826bccf10222804b306f119d6920d4252
Sandro0330 <60124712+Sandro0330@users.noreply.github.com>
2020-10-22 13:24:44 -0300

    Somando os valores da Movimentação

--- src/main/java/br/com/sandro/jpa/teste/TestaSomaMovimentacoes.java
+++ src/main/java/br/com/sandro/jpa/teste/TestaSomaMovimentacoes.java
@@ -1,5 +1,24 @@
 package br.com.sandro.jpa.teste;
 
+import java.math.BigDecimal;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+
 public class TestaSomaMovimentacoes {
 
+	public static void main(String[] args) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("contas");
+		EntityManager em = emf.createEntityManager();
+		
+		String jpql = "select sum(m.valor) from Movimentacao m ";
+		
+		TypedQuery<BigDecimal> query = em.createQuery(jpql, BigDecimal.class);
+		BigDecimal somaDasMovimentacoes = query.getSingleResult();
+		
+		System.out.println("A soma das movimentações é: " + somaDasMovimentacoes);
+		
+	}
 }
