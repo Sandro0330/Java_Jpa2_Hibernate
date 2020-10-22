cf2918a081b3ab24f28a1121c0386cf724eeeb10
Sandro0330 <60124712+Sandro0330@users.noreply.github.com>
2020-10-22 16:31:07 -0300

    Criando A classe MediaComData e alterando o tipo Object para um tipo especifico

--- src/main/java/br/com/sandro/jpa/teste/TestaMediaDiariaDasMovimentacoes.java
+++ src/main/java/br/com/sandro/jpa/teste/TestaMediaDiariaDasMovimentacoes.java
@@ -5,7 +5,9 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+
+import br.com.sandro.jpa.modelo.MediaComData;
 
 public class TestaMediaDiariaDasMovimentacoes {
 
@@ -13,14 +15,14 @@ public class TestaMediaDiariaDasMovimentacoes {
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("contas");
 		EntityManager em = emf.createEntityManager();
 		
-		String jpql = "select avg(m.valor), year(m.data), month(m.data), day(m.data) from Movimentacao m group by  year(m.data), month(m.data), day(m.data)"; // média dos valores da tabela movimentação
+		String jpql = "select new br.com.sandro.jpa.modelo.MediaComData (avg(m.valor), year(m.data), month(m.data), day(m.data)) from Movimentacao m group by  year(m.data), month(m.data), day(m.data)"; // média dos valores da tabela movimentação
 		
-		Query query = em.createQuery(jpql);
-		List<Object[]> mediaDiariaMovimentacoes = query.getResultList();
+		TypedQuery<MediaComData> query = em.createQuery(jpql, MediaComData.class);
+		List<MediaComData> mediaDiariaMovimentacoes = query.getResultList();
 		
-		for (Object[] resultado : mediaDiariaMovimentacoes) {
+		for (MediaComData resultado : mediaDiariaMovimentacoes) {
 			
-			System.out.println("A média das movimentações do dia  " + resultado[1] +  "/" + resultado[2] + "/" + resultado[3] + " é: " + resultado[0]);
+			System.out.println("A média das movimentações do dia  " + resultado.getDia() +  "/" + resultado.getMes() + "/" + resultado.getAno() + " é: " + resultado.getValor());
 		}
 		
 		
