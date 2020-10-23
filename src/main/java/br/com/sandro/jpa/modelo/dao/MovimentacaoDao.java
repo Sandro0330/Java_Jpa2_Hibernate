09b008c706b2db6bc611531bc2986aca6ff872b7
Sandro0330 <60124712+Sandro0330@users.noreply.github.com>
2020-10-23 14:05:25 -0300

    Melhorando o código Dao e inserindo Injeção de dependências

--- src/main/java/br/com/sandro/jpa/modelo/dao/MovimentacaoDao.java
+++ src/main/java/br/com/sandro/jpa/modelo/dao/MovimentacaoDao.java
@@ -4,28 +4,26 @@ import java.math.BigDecimal;
 import java.util.List;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 import javax.persistence.TypedQuery;
 
 import br.com.sandro.jpa.modelo.MediaComData;
 
 public class MovimentacaoDao {
-	public List<MediaComData> getMediaDiariaDasMovimentacoes() {
-		
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("contas");
-		EntityManager em = emf.createEntityManager();
+	private	EntityManager em;
+
+	public MovimentacaoDao(EntityManager em) {
+		this.em = em;
+	}
+
+	public List<MediaComData> getMediaDiariaDasMovimentacoes() {	
 		
 		String jpql = "select new br.com.sandro.jpa.modelo.MediaComData (avg(m.valor), year(m.data), month(m.data), day(m.data)) from Movimentacao m group by  year(m.data), month(m.data), day(m.data)"; // média dos valores da tabela movimentação
 		
 		TypedQuery<MediaComData> query = em.createQuery(jpql, MediaComData.class);
 		return query.getResultList();
-
 	}
 	
 	public BigDecimal getSomaDasMovimentacoes() {
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("contas");
-		EntityManager em = emf.createEntityManager();
 		
 		//String jpql = "select sum(m.valor) from Movimentacao m "; // somando os valores da tabela 
 		String jpql = "select sum(m.valor) from Movimentacao m "; // média dos valores da tabela movimentação
--- src/main/java/br/com/sandro/jpa/teste/TestaSomaMovimentacoes.java
+++ src/main/java/br/com/sandro/jpa/teste/TestaSomaMovimentacoes.java
@@ -1,12 +1,19 @@
 package br.com.sandro.jpa.teste;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import br.com.sandro.jpa.modelo.dao.MovimentacaoDao;
 
 public class TestaSomaMovimentacoes {
 
 	public static void main(String[] args) {
 		
-		MovimentacaoDao dao = new MovimentacaoDao();
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("contas");
+		EntityManager em = emf.createEntityManager();
+		
+		MovimentacaoDao dao = new MovimentacaoDao(em);
 		
 		//System.out.println("A soma das movimentações é: " + somaDasMovimentacoes);
 		System.out.println("A média das movimentações é: " + dao.getSomaDasMovimentacoes());
